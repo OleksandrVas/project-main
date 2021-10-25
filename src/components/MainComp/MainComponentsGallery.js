71686fcb4a40cf46e3bceebc3fa93772e0ebccc7
Oleksand <sasha228123@gmail.com>
2021-10-25 19:35:57 +0300

    added fifth element on the Gallery

--- src/components/MainComp/MainComponentsGallery.js
+++ src/components/MainComp/MainComponentsGallery.js
@@ -108,9 +108,33 @@ const MainComponentsGallery = () => {
 
 
                 </div>
+                <div className="col-xs-12 ">
+                    <div id={classes.fouthId}>
+                        <div className="container">
+                        <div className="row">
+                            <div className="col-xs-6">
+                                <div className={classes.boxImg} ><img
+                                    src={"https://themes.muffingroup.com/be/stylist/wp-content/uploads/2017/12/stylist-gallery-woman2.jpg"}
+                                    className={classes.img} id={classes.fouthImg} /></div>
+
+                            </div>
+                            <div className="col-xs-6">
+                                <div className={classes.boxInfo} id={classes.fouth}>
+                                    <GalleryBox
+                                        number="05"
+                                        text="Duis dignissim mi ut laoreet mollis. Nunc id tellus finibus, eleifend mi vel, maximus justo. Maecenas mi tortor, pellentesque a aliquam ut, fringilla eleifend lectus."
+                                    />
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                    </div>
+
+                </div>
 
 
-                <div className="col-xs-12"></div>
+
                 <div className="col-xs-12"></div>
             </div>
         </>
